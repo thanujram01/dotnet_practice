43c889cf8cce6a1b4cdda226dfd8ab0e0c41e4c3
thanujram01 <thanujram13@gmail.com>
2025-12-13 13:52:34 +0530

    Const & Type Casting

---
+++ Practice1/Practice1/Const_Pract.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice1
+{
+    internal class Const_Pract
+    {
+        static void Main(string[] args)
+        {
+            const float Pi = 3.14f;
+            Pi = 1;
+        }
+    }
+}
---
+++ Practice1/Practice1/Type_cast_Pract.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice1
+{
+    internal class Type_cast_Pract
+    {
+        public static void Main(string[] args)
+        {
+            // byte < int so there wont be any issue in this casting (implicit)
+            byte i = 1;
+            int b = i;
+
+            // gives error
+            int j = 1;
+            //byte c = j; - gives error
+            byte c = (byte)j; //explicit casting
+
+            int x = 1;
+            float y = x;
+
+            string s = "1";
+            // int s2 = (int)s; this will give an error
+            int i3 = Convert.ToInt32(s);
+            int j3 = int.Parse(s);
+        }
+    }
+}
