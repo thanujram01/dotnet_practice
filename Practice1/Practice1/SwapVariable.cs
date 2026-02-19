fa2737f240e6145358ce6dddb1bf038a793ecf20
thanujram01 <thanujram13@gmail.com>
2026-02-20 00:50:51 +0530

    Swap Variable

---
+++ Practice1/Practice1/SwapVariable.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice1
+{
+    internal class SwapVariable
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Value for X");
+            int x = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter Value for Y");
+            int y = Convert.ToInt32(Console.ReadLine());
+
+            x = x - y;
+            y = x + y;
+            x = y - x;
+
+
+            Console.WriteLine("Swapped Value of X - " + x);
+            Console.WriteLine("Swapped Value of Y - " + y);
+        }
+    }
+}
