010b955f7361bb0ede91ee46c5ab8c509c61aa3a
thanujram01 <thanujram13@gmail.com>
2026-04-14 06:33:51 +0530

    Sum of Sum Series

---
+++ Practice1/Practice1/Sum_SumSeries.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice1
+{
+    internal class Sum_SumSeries
+    {
+        public static void Main(string[] Args)
+        {
+            Console.WriteLine("Enter a Number:");
+            int x = Convert.ToInt32(Console.ReadLine());
+
+            
+            int s_sum = 0;
+
+            for(int i = x; i > 0; i--)
+            {
+                int sum = 0;
+
+                for (int j = x - i; j > 0; j--)
+                {
+                    sum = sum + j;
+                }
+
+                s_sum = s_sum + sum;
+            }
+
+            Console.WriteLine("Sum of Sum Series: " + s_sum);
+
+        }
+    }
+}
